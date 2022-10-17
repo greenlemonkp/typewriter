17ba1c24dc414d44232c246cf04df20a10cc548f
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-17 12:47:49 -0400

    animatedSentence

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+
+    // print the char here
+     process.stdout.write(char);
+
+ 
+   }, delay);
+   delay = delay + 50;
+}
